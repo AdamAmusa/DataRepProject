77de23bf2f022ef236eaf4b16d17d36522fa0fae
Adam Amusa <Adamamusa2004@gmail.com>
2023-12-02 15:47:37 +0000

    video playback function fixed

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,3 +1,4 @@
+/*global YT*/
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -5,45 +6,91 @@ import axios from "axios";
 
 function MainPage() {
 
-const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
+
+    function loadPlayBack(){
+
+        const script = document.createElement('script');
+        script.src = 'https://www.youtube.com/iframe_api';
+        script.async = true;
+
+        script.onload = () => {
+            
+            onYouTubeIframeAPIReady();
+          };
+
+        document.head.appendChild(script);
+
+        
+
+    }
+
+
 
     useEffect(
         () => {
+
+
             const script = document.createElement('script');
             script.src = 'https://www.youtube.com/iframe_api';
             script.async = true;
+
+            script.onload = () => {
+                
+                onYouTubeIframeAPIReady();
+              };
+
+
+            document.head.appendChild(script);
+
             axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDhwBKCmPbeDuUIC4-PjxB9gQYJysdANys&q=joji&type=video&part=snippet')/*Generates a Http request to the url and collects data */
-            //when we receive response pull the data into the setData method
-            .then(
-                (response) => {//response function that sets the http data into the data constant variable using the setData function
-                    setData(response.data)
-                }
-            )
-            //when there is no response catch the error
-            .catch(
-                (error) => {//function that prints out the error when the error is caught
-                    console.log(error);
-                }
-            );
-    }, []
+                //when we receive response pull the data into the setData method
+                .then(
+                    (response) => {//response function that sets the http data into the data constant variable using the setData function
+                        setData(response.data)
+                    }
+                )
+                //when there is no response catch the error
+                .catch(
+                    (error) => {//function that prints out the error when the error is caught
+                        console.log(error);
+                    }
+                );
+                
+        }, []
     );
 
 
+    const onYouTubeIframeAPIReady = ()=> {
+        setTimeout(() => {
+        const player = new window.YT.Player("player", {
+            height: 500,
+            width: 900,
+            videoId: "YWyHZNBz6FE",
+            playerVars: {
+                playersinline: 1,
+                autoplay: 0,
+                controls: 1
+            }
+
+        })
+
+    },1000);
+    }
 
 
-return(
+    return (
 
-    <div>
-        <h1> This is the MainPage</h1>
+        <div>
+            <h1> This is the MainPage</h1>
 
-        <h1>
-            Page Info: {data.pageInfo?.totalResults}
-        </h1>
+            <div id="player-container">
+                <div id="player"></div>
+            </div>
 
-       
-    </div>
+        </div>
 
-)
+    )
 
 
 }
