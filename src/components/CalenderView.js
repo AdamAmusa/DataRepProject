cbb7bdadb81b0cbb45e07a853f64f2972f3773b2
Adam Amusa <Adamamusa2004@gmail.com>
2023-12-12 14:21:45 +0000

    Submission of data to console

---
+++ src/components/CalenderView.js
@@ -0,0 +1,82 @@
+import Calendar from 'react-calendar';
+import { useState } from "react";
+import 'react-calendar/dist/Calendar.css';
+import '.././Calender.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { CloseButton } from 'react-bootstrap';
+
+function CalenderView() {
+    const [day, onChange] = useState(new Date());
+    const [showForm, setShowForm] = useState(false);
+
+    const [event, setEvent] = useState('');
+    const [time, setTime] = useState('');
+    
+    const date = day.getDate()
+    const month = day.getMonth() + 1
+    const year = day.getFullYear();
+
+    const clickDay = () => {
+        setShowForm(true)
+    }
+    const handleCloseForm = () => {
+        setShowForm(false);
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let formatDay = `${date}-${month}-${year}`;
+    
+
+       
+
+        console.log("Day: " + formatDay +
+        " Event: " + event +
+            " Time: " + time);
+
+            
+            setShowForm(false);
+    }
+
+
+    return (
+
+        //https://www.npmjs.com/package/react-calendar
+        <div className='centered-container'>
+            <div className='centered-content'>
+                <Calendar onChange={onChange} value={day} onClickDay={clickDay} />
+            </div>
+
+            <div>
+                {/* Render the form when showForm is true */}
+                {showForm && (
+                    <Form onSubmit={handleSubmit} >
+                        <div className="position-relative">
+                            <CloseButton onClick={handleCloseForm} className="position-absolute top-5 end-0"></CloseButton>
+                        </div>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Event</Form.Label>
+                            <Form.Control onChange={(e) => { setEvent(e.target.value) }} value={event} placeholder='Enter event' />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formBasicPassword'>
+                            <Form.Label>Time</Form.Label>
+                            <Form.Control onChange={(e) => { setTime(e.target.value) }} value={time} type="time" placeholder='Enter Time' className="modalTextField" style={{ paddingRight: "6px" }} /> {/*Stack Overflow: https://stackoverflow.com/questions/68283103/how-to-change-colour-of-react-bootstrap-time-picker*/}
+                        </Form.Group>
+                        <Button variant='primary' type='submit'>
+                            Submit
+                        </Button>
+                    </Form>
+                )}
+            </div>
+        </div>
+
+
+
+    )
+
+}
+
+export default CalenderView;
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,82 +1,29 @@
 /*global YT*/
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Calander from "./CalenderView";
 
 
 
 function MainPage() {
 
-    const [data, setData] = useState([]);
-
-    function loadPlayBack(){
-
-        const script = document.createElement('script');
-        script.src = 'https://www.youtube.com/iframe_api';
-        script.async = true;
-
-        script.onload = () => {
-            
-            onYouTubeIframeAPIReady();
-          };
-
-        document.head.appendChild(script);
-
-        
-
-    }
-
+    //const [data, setData] = useState([]);
 
 
     useEffect(
         () => {
 
+            
 
-            const script = document.createElement('script');
-            script.src = 'https://www.youtube.com/iframe_api';
-            script.async = true;
+        },
 
-            script.onload = () => {
-                
-                onYouTubeIframeAPIReady();
-              };
+        []
 
 
-            document.head.appendChild(script);
 
-            axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDhwBKCmPbeDuUIC4-PjxB9gQYJysdANys&q=joji&type=video&part=snippet')/*Generates a Http request to the url and collects data */
-                //when we receive response pull the data into the setData method
-                .then(
-                    (response) => {//response function that sets the http data into the data constant variable using the setData function
-                        setData(response.data)
-                    }
-                )
-                //when there is no response catch the error
-                .catch(
-                    (error) => {//function that prints out the error when the error is caught
-                        console.log(error);
-                    }
-                );
-                
-        }, []
     );
 
 
-    const onYouTubeIframeAPIReady = ()=> {
-        setTimeout(() => {
-        const player = new window.YT.Player("player", {
-            height: 500,
-            width: 900,
-            videoId: "YWyHZNBz6FE",
-            playerVars: {
-                playersinline: 1,
-                autoplay: 0,
-                controls: 1
-            }
-
-        })
-
-    },1000);
-    }
 
 
     return (
@@ -84,9 +31,7 @@ function MainPage() {
         <div>
             <h1> This is the MainPage</h1>
 
-            <div id="player-container">
-                <div id="player"></div>
-            </div>
+            <Calander></Calander>
 
         </div>
 
